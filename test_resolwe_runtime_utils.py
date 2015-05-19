8f031e417bf7698025d0b52c4712cb1c7016abfe
Domen Blenkuš <domen@blenkus.com>
2015-05-19 14:47:50 +0200

    Add more functions.
    Add warning(), error(), info(), progress() and checkrc().
    Add unit tests for the new functions.

--- test_resolwe_runtime_utils.py
+++ test_resolwe_runtime_utils.py
@@ -2,7 +2,9 @@
 import json
 from unittest import TestCase
 
-from resolwe_runtime_utils import save, save_file  # noqa pylint: disable=import-error
+from resolwe_runtime_utils import (
+        save, save_file, error, warning, info, progress, checkrc
+    )
 
 
 class ResolweRuntimeUtilsTestCase(TestCase):
@@ -50,3 +52,61 @@ class TestGenSaveFile(ResolweRuntimeUtilsTestCase):
 
     def test_improper_input(self):
         self.assertRaises(TypeError, save_file, 'etc')
+
+
+class TestGenInfo(ResolweRuntimeUtilsTestCase):
+
+    def test_string(self):
+        self.assertEqual(info('Some info'), '{"proc.info": "Some info"}')
+
+    def test_improper_input(self):
+        self.assertRaises(TypeError, info, 'First', 'Second')
+
+
+class TestGenWarning(ResolweRuntimeUtilsTestCase):
+
+    def test_string(self):
+        self.assertEqual(warning('Some warning'), '{"proc.warning": "Some warning"}')
+
+    def test_improper_input(self):
+        self.assertRaises(TypeError, warning, 'First', 'Second')
+
+
+class TestGenError(ResolweRuntimeUtilsTestCase):
+
+    def test_string(self):
+        self.assertEqual(error('Some error'), '{"proc.error": "Some error"}')
+
+    def test_improper_input(self):
+        self.assertRaises(TypeError, error, 'First', 'Second')
+
+
+class TestGenProgress(ResolweRuntimeUtilsTestCase):
+
+    def test_number(self):
+        self.assertEqual(progress(0.1), '{"proc.progress": 0.1}')
+        self.assertEqual(progress(0), '{"proc.progress": 0}')
+        self.assertEqual(progress(1), '{"proc.progress": 1}')
+
+    def test_string(self):
+        self.assertEqual(progress('0.1'), '{"proc.progress": 0.1}')
+
+    def test_improper_input(self):
+        self.assertRaises(ValueError, progress, 'one')
+        self.assertRaises(ValueError, progress, -1)
+        self.assertRaises(ValueError, progress, 1.1)
+
+
+class TestGenCheckRC(ResolweRuntimeUtilsTestCase):
+
+    def test_valid_integers(self):
+        self.assertEqual(checkrc(0), '{"proc.rc": 0}')
+        self.assertEqual(checkrc(2, 2, "Error"), '{"proc.rc": 0}')
+        self.assertJSONEqual(checkrc(1, 2, "Error"), '{"proc.rc": 1, "proc.error": "Error"}')
+        self.assertEqual(checkrc(2, 2), '{"proc.rc": 0}')
+        self.assertEqual(checkrc(1, 2), '{"proc.rc": 1}')
+
+    def test_valid_strings(self):
+        self.assertEqual(checkrc('0'), '{"proc.rc": 0}')
+        self.assertEqual(checkrc('2', '2', "Error"), '{"proc.rc": 0}')
+        self.assertJSONEqual(checkrc('1', '2', "Error"), '{"proc.rc": 1, "proc.error": "Error"}')
