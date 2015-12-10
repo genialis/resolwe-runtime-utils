400ddc5f760bfb015acb2fb1ec69709224830a0f
Tadej Janež <tadej.j@nez.si>
2015-12-10 11:39:41 +0100

    Create Python package.
    Add licensing and copyright files.
    Add documentation and put it on ReadTheDocs.
    Add support for multiplatform console scripts.
    Add Tox configuration.

--- resolwe_runtime_utils.py
+++ resolwe_runtime_utils.py
@@ -1,3 +1,17 @@
+# Copyright 2015 The resolwe-runtime-utils authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Utility functions that make it easier to write a Resolwe process.
 """
@@ -110,3 +124,42 @@ def checkrc(rc, *args):
         ret['proc.error'] = error_msg
 
     return json.dumps(ret)
+
+
+###############################################################################
+# Auxiliary functions for preparing multi-platform console scripts via        #
+# setuptools' 'console_scripts' entry points mechanism for automatic script   #
+# creation.                                                                   #
+###############################################################################
+
+def _re_generic_main(fn):
+    import sys
+    print(fn(*sys.argv[1:]))
+
+
+def _re_save_main():
+    _re_generic_main(save)
+
+
+def _re_save_file_main():
+    _re_generic_main(save_file)
+
+
+def _re_warning_main():
+    _re_generic_main(warning)
+
+
+def _re_error_main():
+    _re_generic_main(error)
+
+
+def _re_info_main():
+    _re_generic_main(info)
+
+
+def _re_progress_main():
+    _re_generic_main(progress)
+
+
+def _re_checkrc_main():
+    _re_generic_main(checkrc)
