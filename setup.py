4d0e14a34fafdce28810bd42352c0676490bf082
Miha Stajdohar <miha.stajdohar@gmail.com>
2019-04-03 12:29:39 +0200

    Drop support for Python 3.4 and 3.5

--- setup.py
+++ setup.py
@@ -51,8 +51,6 @@ setuptools.setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
